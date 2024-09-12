b1596880fd2e509cc4844cd1357443d2323e0ce8
BRENDAGAVIRIA <160240334+BRENDAGAVIRIA@users.noreply.github.com>
2024-09-12 12:11:00 -0500

    Add files via upload

---
+++ GUI/src/Principal/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Principal;
+
+import Formularios.FrmEstudiantes;
+
+/**
+ *
+ * @author Sala de Sistemas
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmEstudiantes E = new FrmEstudiantes();
+        E.setVisible(true);
+    }
+    
+}
