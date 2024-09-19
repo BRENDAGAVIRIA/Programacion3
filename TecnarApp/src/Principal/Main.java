c8985cde9b03842c90f00916ef5567a79177db5e
BRENDAGAVIRIA <160240334+BRENDAGAVIRIA@users.noreply.github.com>
2024-09-19 11:59:57 -0500

    Add files via upload

---
+++ TecnarApp/src/Principal/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Principal;
+
+import Formularios.FrmLogin;
+
+/**
+ *
+ * @author Sala de Sistemas
+ */
+public class Main {
+    public static void main(String[] args) {
+        FrmLogin L = new FrmLogin();
+        L.setVisible(true);
+        L.setLocationRelativeTo(null);
+    }
+}
+
